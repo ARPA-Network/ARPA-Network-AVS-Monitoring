5c3bb5c22af9c1ec9d3063224c0794d4bb5d1e21
Zuoyuan Zhao <zuoyuan.zhao@arpachain.io>
2024-06-26 21:40:24 -0500

    added file processing

--- custom-exporter/exporter.py
+++ custom-exporter/exporter.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
 import platform
+import os
+import yaml
 from time import sleep
 from eth_account import Account
 from web3 import Web3
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from prometheus_client import start_http_server, Counter, REGISTRY, Enum
+from prometheus_client import start_http_server, Counter, REGISTRY, Info
+
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
     
@@ -20,15 +24,45 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_error(404)
             request_counter.labels(status_code='404', instance=platform.node()).inc()
 
-def get_address(private_key):
+
+def get_address_from_keystore(keystore_path, password):
+    with open(keystore_path) as keyfile:
+        encrypted_key = keyfile.read()
+        keystore_json = json.loads(encrypted_key)
+    
+    private_key = Account.decrypt(keystore_json, password)
+    
     account = Account.from_key(private_key)
+    
     return account.address
+
+
+def get_address():
+    config_path = 'config.yml'
+    keystore_path = 'node.keystore'
+    password = ''
+    private_key = ''
+    if os.path.exists(config_path):
+        with open(config_path, 'r') as file:
+            data = yaml.safe_load(file)
+            if 'account' in data and 'private_key' in data['account']:
+                private_key = data['account']['private_key']
+            elif 'account' in data and 'keystore' in data['account'] and 'password' in data['account']['keystore']:
+                password = data['account']['keystore']['password']
+    
+    # Keystore takes higher priority
+    if os.path.exists(keystore_path) and password != '':
+        return get_address_from_keystore(keystore_path, password)
+    
+    return Account.from_key(private_key)
     
                 
 if __name__ == '__main__':
-    # print(get_address(''))
+    address = get_address()
+    print('address:' + address)
     start_http_server(9000)
     request_counter = Counter('http_requests', 'HTTP request', ["status_code", "instance"])
-
+    address_info = Info('node_address', 'Node address of current node client')
+    address_info.info({'node_address': address})
     webServer = HTTPServer(("localhost", 8080), HTTPRequestHandler).serve_forever()
     print("Server started")
